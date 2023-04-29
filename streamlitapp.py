0f6722282e4780ff242d2d457a52a9df8dd4c000
DmitryKorzhov <110081614+DmitryKorzhov@users.noreply.github.com>
2023-04-29 16:34:56 +0300

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -14,19 +14,22 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
+def display_conversation_history():
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
 def get_text():
     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     return input_text 
 
-if st.session_state['generated']:
-
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+display_conversation_history()
 
 user_input = get_text()
 
-if user_input:
+if st.button("Send"):
     output = "I don't know"
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
+    display_conversation_history()

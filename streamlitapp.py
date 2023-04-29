28fdbaca7078885a256573de1c4fd39652191e52
DmitryKorzhov <110081614+DmitryKorzhov@users.noreply.github.com>
2023-04-29 16:33:24 +0300

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -18,15 +18,15 @@ def get_text():
     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     return input_text 
 
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
 user_input = get_text()
 
 if user_input:
     output = "I don't know"
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
-
-if st.session_state['generated']:
-
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
